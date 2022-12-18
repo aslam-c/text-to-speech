9c72a600566c2cdafaedd73b6a21042bc0136191
aslam <47080903+aslam-c@users.noreply.github.com>
2022-12-18 12:30:03 +0530

    versions change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_pdf_reader",
-    version="0.0.1",
+    version="0.0.2",
     author="aslam",
     author_email="asdmail045@gmail.com",
     description="Reads text from PDF document and save it as an audio file",
--- src/main.py
+++ src/main.py
@@ -1,19 +1,25 @@
 import PyPDF2
 import pyttsx3
 
-def pdfToText():
-    pdfreader = PyPDF2.PdfReader(open("./files/python.pdf", "rb"))
+
+# converts pdf document to audible mp3
+# arguments
+# documentPath : from where the input is to be taken
+# outputAudioPath : audio output file
+# voiceSelection : 1 = female (default) , 0 = male
+
+def pdfToText(documentPath, outputAudioPath, voiceSelection=1):
+    pdfreader = PyPDF2.PdfReader(open(documentPath, "rb"))
     speaker = pyttsx3.init()
     for page_num in range(pdfreader.numPages):
         text = pdfreader.getPage(page_num).extractText()
         clean_text = text.strip().replace('\n', ' ')
         print(clean_text)
     voices = speaker.getProperty('voices')
-    speaker.setProperty('voice', voices[1].id)
+    speaker.setProperty('voice', voices[voiceSelection].id)
     speaker.say("Saving content,please wait ")
-    speaker.save_to_file(clean_text, './files/python.mp3')
+    speaker.save_to_file(clean_text, outputAudioPath)
     speaker.runAndWait()
     speaker.stop()
 
-
-pdfToText()
+pdfToText("./files/inputs/python.pdf", './files/outputs/audio.mp3', 0)
